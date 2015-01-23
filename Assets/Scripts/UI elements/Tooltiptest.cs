33aa75561acb10c67fefb8ffa09e5585c56d1368
Richard Goodwins <richard@pixotic.org>
2015-01-23 22:02:56 +0000

    Tooltip tweaks, first of many

--- Assets/Scripts/UI elements/Tooltiptest.cs
+++ Assets/Scripts/UI elements/Tooltiptest.cs
@@ -2,10 +2,22 @@
 using System.Collections;
 
 public class Tooltiptest : MonoBehaviour {
+
+    protected MonsterObject monsterObject;
+    public string entityName;
+    public string entitySubname;
+
+    // Use this for initialization
+    void Start() {      
+        monsterObject = GetComponent<MonsterObject>();
+        entityName = monsterObject.monsterName;
+        entitySubname = monsterObject.monsterSubname;
+    }
+
 	void OnGUI() {
-		GUI.Box(new Rect(5, 35, 110, 75), new GUIContent("Box", "this box has a tooltip"));
-		GUI.Button(new Rect(10, 55, 100, 20), "No tooltip here");
-		GUI.Button(new Rect(10, 80, 100, 20), new GUIContent("I have a tooltip", "The button overrides the box"));
+		GUI.Box(new Rect(5, 35, 200, 75), new GUIContent(entityName, entitySubname));
+		//GUI.Button(new Rect(10, 55, 100, 20), "No tooltip here");
+		//GUI.Button(new Rect(10, 80, 100, 20), new GUIContent("I have a tooltip", "The button overrides the box"));
 		GUI.Label(new Rect(10, 40, 100, 40), GUI.tooltip);
 	}
 }
