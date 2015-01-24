022dfb66e6fccaf9b1a3c9d896ecaaa071bafe68
lokenwow <lokenwow@gmail.com>
2015-01-24 09:55:38 +0000

    Revert "Revert "tooltips""
    This reverts commit 7e1252864b809cae906c1bf4a82c5aa2d40c3631.

---
+++ Assets/Scripts/UI elements/Tooltiptest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tooltiptest : MonoBehaviour {
+	void OnGUI() {
+		GUI.Box(new Rect(5, 35, 110, 75), new GUIContent("Box", "this box has a tooltip"));
+		GUI.Button(new Rect(10, 55, 100, 20), "No tooltip here");
+		GUI.Button(new Rect(10, 80, 100, 20), new GUIContent("I have a tooltip", "The button overrides the box"));
+		GUI.Label(new Rect(10, 40, 100, 40), GUI.tooltip);
+	}
+}
